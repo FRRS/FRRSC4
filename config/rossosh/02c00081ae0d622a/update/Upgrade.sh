ea372d5d5bcc40f2299800970b6599deb48dd3f9
FRRS <404018@mail.de>
2019-09-19 07:10:51 +0300

    Update Upgrade.sh

--- config/rossosh/02c00081ae0d622a/update/Upgrade.sh
+++ config/rossosh/02c00081ae0d622a/update/Upgrade.sh
@@ -2,5 +2,5 @@
 ##Updating sript for FRRSC4 Ver.0.0.1.Bild.001
 date > //scr_io/upgrade.txt
 cd //etc/cron.d
-sudo wget -O $source https://raw.githubusercontent.com/FRRS/FRRSC4/master/config/rossosh/$HOST/update/GSMinfotages
+sudo wget -O https://raw.githubusercontent.com/FRRS/FRRSC4/master/config/rossosh/$HOST/update/GSMinfotages
 echo "Готово"
