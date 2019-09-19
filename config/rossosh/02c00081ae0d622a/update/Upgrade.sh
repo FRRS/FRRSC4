db4626ba988fb2a055394f85f8f64979d6ea47ea
FRRS <404018@mail.de>
2019-09-19 10:21:39 +0300

    Update Upgrade.sh
    Co-Authored-By: lemingg <lemingg@users.noreply.github.com>
    Co-Authored-By: ppbarkov <ppbarkov@users.noreply.github.com>

--- config/rossosh/02c00081ae0d622a/update/Upgrade.sh
+++ config/rossosh/02c00081ae0d622a/update/Upgrade.sh
@@ -3,4 +3,5 @@
 date > //scr_io/upgrade.txt
 cd //etc/cron.d
 sudo wget -O GSMinfotages https://raw.githubusercontent.com/FRRS/FRRSC4/master/config/rossosh/$HOST/update/GSMinfotages
+sudo /etc/init.d/cron restart
 echo "Готово"
