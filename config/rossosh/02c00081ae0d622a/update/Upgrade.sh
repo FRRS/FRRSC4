5c14aeb0ff14a0fea32d6e07aacc673a9ca6fbc5
lemingg <37049114+lemingg@users.noreply.github.com>
2019-09-19 12:25:27 +0700

    Update Upgrade.sh

--- config/rossosh/02c00081ae0d622a/update/Upgrade.sh
+++ config/rossosh/02c00081ae0d622a/update/Upgrade.sh
@@ -2,6 +2,6 @@
 ##Updating sript for FRRSC4 Ver.0.0.1.Bild.001
 date > //scr_io/upgrade.txt
 cd //etc/cron.d
-sudo wget -O https://raw.githubusercontent.com/FRRS/FRRSC4/master/config/rossosh/$HOST/update/GSMinfotages
+sudo wget -O GSMinfotages https://raw.githubusercontent.com/FRRS/FRRSC4/master/config/rossosh/$HOST/update/GSMinfotages
 echo "Готово"
 ##
