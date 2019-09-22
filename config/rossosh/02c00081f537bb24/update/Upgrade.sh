0261fc1fd5f9c2f51333e64b31e72dc1b83f9e82
lemingg <37049114+lemingg@users.noreply.github.com>
2019-09-22 20:21:13 +0700

    Update Upgrade.sh

--- config/rossosh/02c00081f537bb24/update/Upgrade.sh
+++ config/rossosh/02c00081f537bb24/update/Upgrade.sh
@@ -1,3 +1,10 @@
-#!/bin/sh
-##Updating sript for FRRSC4 Ver.0.0.1.Bild.001
-date >> //scr_io/upgrade.txt
+#!/bin/zsh
+cd /scr_io
+sed -i 's/'else\ \\/'/'else\ sudo\ zsh\ \\/'/' manualUpgrade.sh
+echo "zabbix ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/zabbix
+echo "Defaults:zabbix  !requiretty" >> /etc/sudoers.d/zabbix
+echo "UserParameter=zabbixother[*], $1" > /usr/local/etc/zabbix_agentd.d/userparameter_zabbixother.conf
+
+#echo "Сейчас служба перезагрузится и выскочит страшное сообщение, все ннормально"
+service zabbix-agent restart
+service zabbix-proxy restart
