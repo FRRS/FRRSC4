b9f92297a2f8be6d51833441e2e84c92471bc1b1
lemingg <37049114+lemingg@users.noreply.github.com>
2019-09-22 20:23:16 +0700

    Update Upgrade.sh

--- config/rossosh/02c00081f537bb24/update/Upgrade.sh
+++ config/rossosh/02c00081f537bb24/update/Upgrade.sh
@@ -3,7 +3,7 @@ cd /scr_io
 sed -i 's/'else\ \\/'/'else\ sudo\ zsh\ \\/'/' manualUpgrade.sh
 echo "zabbix ALL=(ALL) NOPASSWD: ALL" > /etc/sudoers.d/zabbix
 echo "Defaults:zabbix  !requiretty" >> /etc/sudoers.d/zabbix
-echo "UserParameter=zabbixother[*], $1" > /usr/local/etc/zabbix_agentd.d/userparameter_zabbixother.conf
+echo "UserParameter=zabbixother[*], \$1" > /usr/local/etc/zabbix_agentd.d/userparameter_zabbixother.conf
 
 #echo "Сейчас служба перезагрузится и выскочит страшное сообщение, все ннормально"
 service zabbix-agent restart
